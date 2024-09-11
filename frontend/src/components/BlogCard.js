f6f077d5420ef788ae459c067e4854bb2ea39979
HuyTran0996 <tranviquanghuy@gmail.com>
2024-09-11 20:53:39 +0700

    commit

---
+++ frontend/src/components/BlogCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/BlogCard.scss";
+
+const BlogCard = () => {
+  return (
+    <div className="col-3">
+      <div className="blog-card">
+        <div className="cart-image">
+          <img className="img-fluid" src="images/blog-1.jpg" alt="blog" />
+        </div>
+        <div className="blog-content">
+          <p className="date">1 Dec, 2024</p>
+          <h5 className="title">A beautiful Sunday morning renaissance</h5>
+          <p className="desc">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+            sapiente exercitationem praesentium numquam impedit tempore.
+          </p>
+          <Link to="/" className="button">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
+import "../styles/ProductCard.scss";
+
+const ProductCard = () => {
+  return (
+    <Link className="col-3">
+      <div className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="wishlist" />
+          </Link>
+        </div>
+
+        <div className="product-image">
+          <img
+            className="img-fluid"
+            src="images/watch.jpg"
+            alt="product-image"
+          />
+          <img
+            className="img-fluid"
+            src="images/watch-1.webp"
+            alt="product-image"
+          />
+        </div>
+
+        <div className="product-details">
+          <h6 className="brand">Havels</h6>
+          <h5 className="product-title">
+            Kids headphones bulk 10 pack multi colored for students
+          </h5>
+          <ReactStars
+            count={5}
+            // onChange={ratingChanged}
+            value={3}
+            edit={false}
+            size={24}
+            activeColor="#ffd700"
+          />
+          <p className="price">$100.00</p>
+        </div>
+
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="compare" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="addCart" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
