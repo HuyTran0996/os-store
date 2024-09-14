baf843b8d23f064b3f73c79f3a828ea35b824ed4
HuyTran0996 <tranviquanghuy@gmail.com>
2024-09-14 15:04:28 +0700

    2024-09-14 Cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import RefundPolicy from "./pages/RefundPolicy";
 import ShippingPolicy from "./pages/ShippingPolicy";
 import TermAndConditions from "./pages/TermAndConditions";
 import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -28,17 +29,22 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+
           <Route path="product" element={<OurStore />} />
           <Route path="product/:id" element={<SingleProduct />} />
 
           <Route path="blogs" element={<Blog />} />
           <Route path="blog/:id" element={<SingleBlog />} />
+
+          <Route path="cart" element={<Cart />} />
           <Route path="compare-product" element={<CompareProduct />} />
           <Route path="wishlist" element={<Wishlist />} />
+
           <Route path="login" element={<Login />} />
           <Route path="forgot-password" element={<Forgotpassword />} />
           <Route path="signup" element={<Signup />} />
           <Route path="reset-password" element={<Resetpassword />} />
+
           <Route path="privacy-policy" element={<PrivacyPolicy />} />
           <Route path="refund-policy" element={<RefundPolicy />} />
           <Route path="shipping-policy" element={<ShippingPolicy />} />
