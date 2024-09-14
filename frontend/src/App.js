45cd65df7d406ac9044f5a9f4afb4f13b514d5a7
HuyTran0996 <tranviquanghuy@gmail.com>
2024-09-14 16:55:59 +0700

    2024-09-14 checkout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import ShippingPolicy from "./pages/ShippingPolicy";
 import TermAndConditions from "./pages/TermAndConditions";
 import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
           <Route path="blog/:id" element={<SingleBlog />} />
 
           <Route path="cart" element={<Cart />} />
+          <Route path="checkout" element={<Checkout />} />
           <Route path="compare-product" element={<CompareProduct />} />
           <Route path="wishlist" element={<Wishlist />} />
 
