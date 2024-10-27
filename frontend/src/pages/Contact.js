c5cca52bf49446de8147b5dd62dd8dd23e9da2d0
HuyTran0996 <tranviquanghuy@gmail.com>
2024-10-27 12:03:13 +0700

    2024-10-27 contact page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,103 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { Box } from "@mui/material";
 
 import "../styles/Contact.scss";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import Container from "../components/Container";
+import { showToast } from "../components/ToastMessage";
 
 import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
 import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
 
+const phone = process.env.REACT_APP_PHONE;
+const email = process.env.REACT_APP_EMAIL;
+const workTime = process.env.REACT_APP_WORK_TIME;
+const address = process.env.REACT_APP_ADDRESS;
+const googleMap = process.env.REACT_APP_LOCATION_MAP;
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    showToast(
+      "The feature is currently under construction. Please return at a later time.",
+      "info",
+      5000
+    );
+  };
+
   return (
     <div className="contactPage">
       <Meta title="Contact Us" />
       <BreadCrumb title="Contact Us" />
-      <Container class1="contact-wrapper py-5">
-        <div className="row">
-          <div className="col-12">
-            <iframe
-              title="Our Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15664.324803631758!2d106.37140174579176!3d11.032534476113739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310b2faf621d6267%3A0x66071ced41e33f34!2zQW4gQsOsbmgsIEFuIFThu4tuaCwgSG8gQ2hpIE1pbmggQ2l0eSwgVMOieSBOaW5oLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1726146049984!5m2!1sen!2s"
-              width="600"
-              height="450"
-              className="border-0 w-100"
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            />
+      <Box className="contact-wrapper">
+        <iframe
+          className="map"
+          title="Our Location"
+          src={googleMap}
+          width="600"
+          height="450"
+          style={{ border: 0, width: "100%" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        />
+
+        <div className="contact-inner-wrapper">
+          <div className="contactForm">
+            <h3 className="contact-title">Contact</h3>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                value={emailAddress}
+                onChange={(e) => setEmailAddress(e.target.value)}
+              />
+
+              <PhoneInput
+                className="form-control"
+                placeholder="Phone number..."
+                value={phoneNumber}
+                onChange={setPhoneNumber}
+              />
+
+              <textarea
+                className="form-control"
+                cols={30}
+                rows={4}
+                placeholder="Message..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+
+              <button type="submit" className="button">
+                Submit
+              </button>
+            </form>
           </div>
 
-          <div className="col-12 mt-5">
-            <div className="contact-inner-wrapper d-flex justify-content-between">
-              <div>
-                <h3 className="contact-title mb-4">Contact</h3>
-                <form action="" className="d-flex flex-column gap-15">
-                  <div>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type="tel"
-                      className="form-control"
-                      placeholder="Mobile Number"
-                    />
-                  </div>
-                  <div>
-                    <textarea
-                      name=""
-                      id=""
-                      className="w-100 form-control"
-                      cols={30}
-                      rows={4}
-                      placeholder="Comment..."
-                    ></textarea>
-                  </div>
-                  <div>
-                    <button className="button border-0">Submit</button>
-                  </div>
-                </form>
-              </div>
-              <div>
-                <h3 className="contact-title mb-4">Get In Touch With Us</h3>
-                <div>
-                  <ul className="ps-0">
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiOutlineHome className="fs-5" />
-                      <address className="mb-0">
-                        No. 1234 An Bình, An Tịnh- Trảng Bàng Tây Ninh- Việt Nam
-                      </address>
-                    </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <BiPhoneCall className="fs-5" />
-                      <a href="tel:+84 123456789">+84 123 456 789</a>
-                    </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiOutlineMail className="fs-5" />
-                      <a href="mailto:test@gmail.com">test@gmail.com</a>
-                    </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <BiInfoCircle className="fs-5" />
-                      <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+          <div className="storeInfo">
+            <h3 className="contact-title">Get In Touch With Us</h3>
+
+            <ul>
+              <li>
+                <AiOutlineHome />
+                <address>{address}</address>
+              </li>
+              <li>
+                <BiPhoneCall />
+                <a href={`tel:${phone}`}>{phone}</a>
+              </li>
+              <li>
+                <AiOutlineMail />
+                <a href={`mailto:${email}`}>{email}</a>
+              </li>
+              <li>
+                <BiInfoCircle />
+                <p>{workTime}</p>
+              </li>
+            </ul>
           </div>
         </div>
-      </Container>
+      </Box>
     </div>
   );
 };
